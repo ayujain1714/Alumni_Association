4d9fe6559fea2d9c53da77a1f277d4dc50a2b0f5
Divyanshu Shekhar <149070310+Divyanshu832@users.noreply.github.com>
2024-09-06 10:43:17 +0530

    Add files via upload

---
+++ src/components/Blog-Page/pages/Write/Write.jsx
@@ -0,0 +1,36 @@
+import "./write.css";
+
+export default function Write() {
+  return (
+    <div className="write">
+      <div  className="flex justify-center">
+      <img className="w-[90%]" src="/assets\images\Blog_Banner.png" alt="" />
+      </div>
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input id="fileInput" type="file" style={{ display: "none" }} />
+          <input
+            className="writeInput"
+            placeholder="Title"
+            type="text"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            className="writeInput writeText"
+            placeholder="Tell your story..."
+            type="text"
+            autoFocus={true}
+          />
+        </div>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
+      </form>
+    </div>
+  );
+}
